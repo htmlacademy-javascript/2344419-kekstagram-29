76ffc1c3d2ace4ba28bb52e0732453097fcc51cc
Keks <keks@htmlacademy.ru>
2023-06-21 16:20:15 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -17,6 +17,7 @@ const NAMES = [
   "Соня",
   "Клава",
   "Максим",
+  "Саша",
 ];
 
 const MESSAGES = [
@@ -59,10 +60,10 @@ const photoComments = (min, max) => {
   }));
 };
 
-const userPhoto = (COUNTOBJ) => ({
-  id: COUNTOBJ,
-  url: `photos/${COUNTOBJ}.jpg`,
-  description: `фотография с ${COUNTOBJ} котиком`,
+const userPhoto = (count) => ({
+  id: count,
+  url: `photos/${count}.jpg`,
+  description: `фотография с ${count} котиком`,
   likes: getRandom(MINLIKES, MAXLIKES),
   comments: photoComments(0, MAXCOMMENTS),
 });
