5a6e056dac446061933cf33744075d84d0ac9890
TyurinaNataliya <112402828+TyurinaNataliya@users.noreply.github.com>
2023-06-25 12:39:57 +0300

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -13,3 +13,24 @@ function isPalindrome(string) {
 
 isValid('Test', 20);
 isPalindrome('тест');
+
+//////////////////////////////////
+
+const workingMeetings = (startDay, endDay, durationHours, duration) => {
+  const getNumberTime = (str) => {
+    let result = 0;
+    const arr = str.split(':');
+    for (let i = 0; i < arr.length; i++) {
+      if (i === 0) {
+        result += arr[i] * 60;
+      } else {
+        result += +arr[i];
+      }
+    }
+    return result;
+  };
+
+  return getNumberTime(durationHours) >= getNumberTime(startDay) &&
+    getNumberTime(durationHours) + duration <= getNumberTime(endDay);
+};
+workingMeetings('8:00', '17:30', '08:00', 900);
