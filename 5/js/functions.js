bca497633b871bde86eaf31c3927d912c4cbc6e5
Keks <keks@htmlacademy.ru>
2023-06-25 12:31:30 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/functions.js
+++ 5/js/functions.js
@@ -3,10 +3,13 @@ function isValid(string, maxLength) {
 }
 
 function isPalindrome(string) {
-  const normalString = string.replaceAll(" ", "").toLowerCase();
-  let reverseString = "";
+  const normalString = string.replaceAll(' ', '').toLowerCase();
+  let reverseString = '';
   for (let i = normalString.length - 1; i >= 0; i--) {
     reverseString += normalString[i];
   }
   return normalString === reverseString;
 }
+
+isValid('Test', 20);
+isPalindrome('тест');
